b8077d020b487232bad23d6b7caf0b980870d2b3
kiyutink <kiyutin@gmail.com>
2022-07-25 00:28:16 +0200

    Add delete method to routes

--- routes.go
+++ routes.go
@@ -4,4 +4,5 @@ func (s *Server) routes() {
 	s.router.Get("/polls", s.pollController.HandleGetMany())
 	s.router.Get("/polls/{id}", s.pollController.HandleGetOne())
 	s.router.Post("/polls", s.pollController.HandlePost())
+	s.router.Delete("/polls", s.pollController.HandleDelete())
 }
