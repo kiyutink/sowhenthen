fa00547ba8e03bee0426a5a0c2493580a97951e3
kiyutink <kiyutin@gmail.com>
2022-07-25 00:18:27 +0200

    Use handlers

--- poll/controller.go
+++ poll/controller.go
@@ -21,40 +21,48 @@ type Controller struct {
 	storer Storer
 }
 
-func (pc *Controller) GetMany(w http.ResponseWriter, r *http.Request) {
-	polls := pc.storer.GetMany()
-	names := []string{}
-	for _, poll := range polls {
-		names = append(names, strconv.Itoa(poll.Id))
+func (pc *Controller) HandleGetMany() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		polls := pc.storer.GetMany()
+		names := []string{}
+		for _, poll := range polls {
+			names = append(names, strconv.Itoa(poll.Id))
+		}
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(strings.Join(names, ", ")))
+		fmt.Println(pc.storer.Dump())
 	}
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(strings.Join(names, ", ")))
-	fmt.Println(pc.storer.Dump())
 }
 
-func (pc *Controller) GetOne(w http.ResponseWriter, r *http.Request) {
-	idParam := chi.URLParam(r, "id")
-	id, _ := strconv.Atoi(idParam)
-	poll := pc.storer.GetOne(id)
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(poll.Title))
-	fmt.Println(pc.storer.Dump())
+func (pc *Controller) HandleGetOne() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		idParam := chi.URLParam(r, "id")
+		id, _ := strconv.Atoi(idParam)
+		poll := pc.storer.GetOne(id)
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(poll.Title))
+		fmt.Println(pc.storer.Dump())
+	}
 }
 
-func (pc *Controller) Post(w http.ResponseWriter, r *http.Request) {
-	pc.storer.Create(Poll{
-		Title: "Just another poll",
-	})
-	w.WriteHeader(http.StatusCreated)
-	fmt.Println(pc.storer.Dump())
+func (pc *Controller) HandlePost() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		pc.storer.Create(Poll{
+			Title: "Just another poll",
+		})
+		w.WriteHeader(http.StatusCreated)
+		fmt.Println(pc.storer.Dump())
+	}
 }
 
-func (pc *Controller) Delete(w http.ResponseWriter, r *http.Request) {
-	idParam := chi.URLParam(r, "id")
-	id, _ := strconv.Atoi(idParam)
-	pc.storer.Delete(id)
-	w.WriteHeader(http.StatusOK)
-	fmt.Println(pc.storer.Dump())
+func (pc *Controller) HandleDelete() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		idParam := chi.URLParam(r, "id")
+		id, _ := strconv.Atoi(idParam)
+		pc.storer.Delete(id)
+		w.WriteHeader(http.StatusOK)
+		fmt.Println(pc.storer.Dump())
+	}
 }
 
 func NewController(storer Storer) *Controller {
--- routes.go
+++ routes.go
@@ -1,7 +1,7 @@
 package main
 
 func (s *Server) routes() {
-	s.router.Get("/polls", s.pollController.GetMany)
-	s.router.Get("/polls/{id}", s.pollController.GetOne)
-	s.router.Post("/polls", s.pollController.Post)
+	s.router.Get("/polls", s.pollController.HandleGetMany())
+	s.router.Get("/polls/{id}", s.pollController.HandleGetOne())
+	s.router.Post("/polls", s.pollController.HandlePost())
 }
