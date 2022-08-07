713ba560c30820ef2db3a849adc96686b8c217af
kiyutink <kiyutin@gmail.com>
2022-08-07 15:36:04 +0200

    Configuration with env

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"os"
 	"time"
 
+	"github.com/joho/godotenv"
 	"github.com/kiyutink/sowhenthen/poll"
 	"github.com/kiyutink/sowhenthen/vote"
 )
@@ -13,14 +15,20 @@ import (
 const timeout = time.Second * 30
 
 func main() {
-	mongoClient, err := newMongoClient("mongodb://localhost:27017")
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+
+	mongoClient, err := newMongoClient(os.Getenv("MONGO_URL"))
 	defer mongoClient.Disconnect(context.Background())
 	if err != nil {
 		panic(err)
 	}
 	srv := NewServer(poll.NewMongoStorage(mongoClient), vote.NewMongoStorage(mongoClient))
 	srv.routes()
-	fmt.Println("listening on localhost:8001")
-	err = http.ListenAndServe("localhost:8001", srv)
+	socket := fmt.Sprintf("%v:%v", os.Getenv("HOST"), os.Getenv("PORT"))
+	fmt.Printf("server listening on socket %v\n", socket)
+	err = http.ListenAndServe(socket, srv)
 	fmt.Println(err)
 }
