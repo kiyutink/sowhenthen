8fa51028f1742836b3956216cdf1e56fb3e746af
kiyutink <kiyutin@gmail.com>
2022-09-03 11:11:03 +0200

    Ping DB after connection

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net/http"
 	"os"
@@ -11,23 +12,27 @@ import (
 	"github.com/kiyutink/sowhenthen/mongo"
 	mongoDB "go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 const timeout = time.Second * 30
 
 func newMongoClient(url string) (*mongoDB.Client, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
 	defer cancel()
 	client, err := mongoDB.Connect(ctx, options.Client().ApplyURI(url))
+	if err != nil {
+		return client, err
+	}
+	err = client.Ping(ctx, readpref.Primary())
 	return client, err
 }
 
 func main() {
 	err := godotenv.Load()
-	if err != nil {
+	if err != nil && !errors.Is(err, os.ErrNotExist) {
 		panic(err)
 	}
-
 	mongoClient, err := newMongoClient(os.Getenv("MONGO_URL"))
 	defer mongoClient.Disconnect(context.Background())
 	if err != nil {
